096e09542855de35a4d4f478a2f5fe2c3725b0d7
HaidarAbas <haidarabas@gmail.com>
2026-06-11 17:14:06 +0800

    Initialize with-biome-formatter

---
+++ src/script.js
@@ -0,0 +1,15 @@
+// This branch utilizes Biome formatter. Please check package.json for the version of Biome being used
+
+// REMINDER!! appened script:deploy in package.json with branch name
+
+const checkMode = () => {
+	if (process.env.NODE_ENV === "development") {
+		console.log("I am in development");
+	} else if (process.env.NODE_ENV === "production") {
+		console.log("I am in production");
+	} else {
+		console.log(process.env.NODE_ENV);
+	}
+};
+
+checkMode();
